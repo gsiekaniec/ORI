86b7ef4492e8f0de4b7c918f19f814aa43d272c0
Teo Lemane <teo.lemane@gmail.com>
2020-12-08 15:12:42 +0100

    pipeline

--- pipeline/index4.sh
+++ pipeline/index4.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-FASTQ="./test.fastq"
-QUAL=9
-LENGTH=2000
-NB_READS=
 
-#ORI.py suppr_bad_reads -fq ${FASTQ} -q ${QUAL} -l ${LENGTH}
-
-BASEN=$(basename -- $FASTQ)
-EXT="${BASEN##*.}"
-FASTQ_F="${BASEN%.*}_better_than_${QUAL}.${EXT}"
-
-
-head -n 16000 ${FASTQ_F} > query_${NB_READS}.fq
+echo "Build index ..."
+howdesbt cluster --list=leafname --tree=union.sbt --nodename=node{number} --cull
+howdesbt build --howde --tree=union.sbt --outtree=howde.sbt
+ls | grep -Pv 'detbrief.rrr.' | grep '.bf' | xargs rm --
+echo "Done"
