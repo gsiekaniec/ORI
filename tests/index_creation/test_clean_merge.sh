8915982794bffb21a9fb64e2a386c0be3e27153e
SIEKANIEC Gregoire <gregoire.siekaniec@inria.fr>
2020-12-23 10:27:01 +0100

    modif tests

--- tests/index_creation/test_clean_merge.sh
+++ tests/index_creation/test_clean_merge.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 ../../ORI.py clean_merge -n leafname -r . -o list_number_file.txt
+ORI.py clean_merge -n leafname -r . -o list_number_file.txt
 
 diff ./list_number_file_test.txt ./list_number_file.txt
